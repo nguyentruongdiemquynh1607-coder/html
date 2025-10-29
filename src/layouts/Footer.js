2ba20d0a08069d20257f0df4c10ce670f68ad374
nguyentruongdiemquynh1607-coder <nguyentruongdiemquynh1607@gmail.com>
2025-10-29 21:10:48 +0700

    Update Footer.js

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,39 +1,40 @@
-const Footer=()=>
-    {
-     
-        return(
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <div class="footer">
+        <div class="container_main">
+          <div class="logo_footer">
+            <a href="">
+              <img
+                src="https://giadinh.edu.vn/upload/photo/logofooter-8814.png"
+                alt="Logo"
+              />
+            </a>
+          </div>
+          <div class="content_footer">
+            <h2> BIGHIT ENTERTAINMENT</h2>
+            <p>Địa chỉ: Yongsan Trade Center, Yongsan-gu, Seoul, Hàn Quốc</p>
 
-            <footer id="footer">
-  <div class="footer">
-    <div class="container_main">
-      <div class="logo_footer">
-        <a href="">
-          <img src="https://giadinh.edu.vn/upload/photo/logofooter-8814.png" alt="Logo"/>
-        </a>
+            <p>Hotline: 0961 12 10 18 - 0962 12 10 18 - 0013 06 20 13</p>
+
+            <p>Điện thoại: (028)7301.1306</p>
+
+            <p>Email: Bighit@gmail.com</p>
+          </div>
+          <div class="banner_footer"></div>
+        </div>
       </div>
-      <div class="content_footer">
-        <h2>TRƯỜNG ĐẠI HỌC GIA ĐỊNH</h2>
-        <p>Cơ sở Tân Sơn Nhất: 371 Nguyễn Kiệm, P.3, Q.Gò Vấp, TP.HCM</p>
-
-<p>Hotline: 0961 12 10 18 - 0962 12 10 18 - 0862 12 10 18</p>
-
-<p>Điện thoại: (028)7301.3456</p>
-
-<p>Email: tuyensinh@giadinh.edu.vn</p>
-      </div> 
-      <div class="banner_footer">
-       
+      <div class="copyright">
+        <div class="container_main">
+          <p>
+            © 2024 Demo bởi thầy Gia.<a> | Sitemap</a>
+          </p>
+          <p>
+            © 2025 Design bởi Diễm Quỳnh.<a> | Sitemap</a>
+          </p>
+        </div>
       </div>
-    </div>
-  </div>
-  <div class="copyright">
-    <div class="container_main">
-      <p>© 2024 Demo bởi thầy Gia.<a> | Sitemap</a></p>
-    </div>
-  </div>
-</footer>
-
-        );
-
-    }
+    </footer>
+  );
+};
 export default Footer;
